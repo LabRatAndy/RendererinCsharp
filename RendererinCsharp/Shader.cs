d8843a286a0d974c23401051074db18e4d9c1824
me <me@LAPTOP>
2018-11-13 21:44:51 +0000

    completed the shader class

--- RendererinCsharp/Shader.cs
+++ RendererinCsharp/Shader.cs
@@ -1,5 +1,5 @@
-﻿using OpenTK;
-using System;
+﻿using System;
+using OpenTK.Graphics.OpenGL4;
 
 namespace RendererinCsharp
 {
@@ -10,31 +10,62 @@ namespace RendererinCsharp
         private int fragmentshader;
         Shader(string vertexshaderfile, string fragmentshaderfile)
         {
-
+            handle = GL.CreateProgram();
+            LoadShader(vertexshaderfile, ShaderType.VertexShader);
+            LoadShader(fragmentshaderfile, ShaderType.FragmentShader);
+            GL.AttachShader(handle, vertexshader);
+            GL.AttachShader(handle, fragmentshader);
+            GL.LinkProgram(handle);
+            string error;
+            error = GL.GetProgramInfoLog(handle);
+            Console.WriteLine(error);
+            GL.DetachShader(handle,vertexshader);
+            GL.DetachShader(handle, fragmentshader);
+            GL.DeleteShader(vertexshader);
+            GL.DeleteShader(fragmentshader);
         }
         public void Use()
         {
-
+            GL.UseProgram(handle);
         }
         public int GetAttibuteHandle(string attributename)
         {
-
+            return GL.GetAttribLocation(handle, attributename);
         }
         public int GetUniformHandle(string uniformname)
         {
-
+            return GL.GetUniformLocation(handle, uniformname);
         }
         public void Dispose()
         {
-
+            GL.DeleteProgram(handle);
+            GC.SuppressFinalize(this);
         }
         ~Shader()
         {
-
+            GL.DeleteProgram(handle);
         }
-        private void LoadShader(string filename,OpenTK.Graphics.OpenGL4.ShaderType shadertype)
+        private void LoadShader(string filename,ShaderType shadertype)
         {
-
+            string shadersource = System.IO.File.ReadAllText(filename);
+            if(shadertype == ShaderType.VertexShader)
+            {
+                vertexshader = GL.CreateShader(shadertype);
+                GL.ShaderSource(vertexshader, shadersource);
+                GL.CompileShader(vertexshader);
+                string error = null;
+                error = GL.GetShaderInfoLog(vertexshader);
+                Console.WriteLine(error);
+            }
+            if(shadertype == ShaderType.FragmentShader)
+            {
+                fragmentshader = GL.CreateShader(shadertype);
+                GL.ShaderSource(fragmentshader, shadersource);
+                GL.CompileShader(fragmentshader);
+                string error = null;
+                error = GL.GetShaderInfoLog(fragmentshader);
+                Console.WriteLine(error);
+            }
         }
     }
 
