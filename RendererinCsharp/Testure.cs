2e6dfb5e03b361fe0583bc424f2290cbbb9d6f36
me <me@LAPTOP>
2018-11-13 21:58:42 +0000

    Defined the teture class needs implementing

---
+++ RendererinCsharp/Testure.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System;
+using OpenTK.Graphics.OpenGL;
+
+namespace RendererinCsharp
+{
+    class Texture : IDisposable
+    {
+        private int handle;
+        private bool cubemapped;
+
+        Texture(string filename)
+        {
+
+        }
+        Texture(string top, string bottom, string left, string right, string front, string back)
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+        ~Texture()
+        {
+
+        }
+        public void Bind()
+        {
+
+        }
+        public void Unbind()
+        {
+
+        }
+        public void Activate(TextureUnit textureunit)
+        {
+
+        }
+        public bool IsCubeMapped
+        {
+            get { return cubemapped; }
+        }
+
+    }
+
+}
