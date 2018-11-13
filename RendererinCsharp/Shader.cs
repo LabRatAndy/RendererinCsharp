ee922c60c6f236368d5251618ec417afdb6c1368
me <me@LAPTOP>
2018-11-13 20:44:27 +0000

    defined Shader class

---
+++ RendererinCsharp/Shader.cs
@@ -0,0 +1,41 @@
+﻿using OpenTK;
+using System;
+
+namespace RendererinCsharp
+{
+    class Shader : IDisposable
+    {
+        private int handle;
+        private int vertexshader;
+        private int fragmentshader;
+        Shader(string vertexshaderfile, string fragmentshaderfile)
+        {
+
+        }
+        public void Use()
+        {
+
+        }
+        public int GetAttibuteHandle(string attributename)
+        {
+
+        }
+        public int GetUniformHandle(string uniformname)
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+        ~Shader()
+        {
+
+        }
+        private void LoadShader(string filename,OpenTK.Graphics.OpenGL4.ShaderType shadertype)
+        {
+
+        }
+    }
+
+}
