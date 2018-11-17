095d8e986fa3e952569fa2b6f907dd55d45bb142
me <me@LAPTOP>
2018-11-17 16:56:18 +0000

    implement VBO class

--- RendererinCsharp/VBO.cs
+++ RendererinCsharp/VBO.cs
@@ -11,31 +11,35 @@ namespace RendererinCsharp
 
         VertexBufferObject(float[] vertexdata, int size)
         {
-
+            GL.GenBuffers(1, out handle);
+            this.vertexData = vertexdata;
+            dataSize = size;
         }
         public void Bind()
         {
-
+            GL.BindBuffer(BufferTarget.ArrayBuffer, handle);
         }
         public void BufferData(BufferUsageHint drawtype)
         {
-
+            GL.BindBuffer(BufferTarget.ArrayBuffer, handle);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertexData.Length * sizeof(float), vertexData, drawtype);
         }
         public void Draw(int first, int count)
         {
-
+            GL.DrawArrays(PrimitiveType.Triangles, first, count);
         }
         public void UnBind()
         {
-
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
         }
         public void Dispose()
         {
-
+            GL.DeleteBuffer(handle);
+            GC.SuppressFinalize(this);
         }
         ~VertexBufferObject()
         {
-
+            GL.DeleteBuffer(handle);
         }
     }
 
