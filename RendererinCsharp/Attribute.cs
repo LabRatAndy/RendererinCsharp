30e0c3fa1498f7f1452ab3d0ea29b3aa9d9481f9
me <me@LAPTOP>
2018-11-17 17:26:37 +0000

    Added Attribute class

---
+++ RendererinCsharp/Attribute.cs
@@ -0,0 +1,40 @@
+﻿using OpenTK.Graphics.OpenGL;
+
+namespace RendererinCsharp
+{
+    public class Attribute
+    {
+        private string name;
+        private int size;
+        private int offset;
+        private int stride;
+        private int index;
+        private VertexAttribPointerType type;
+        private bool normalised;
+        
+        Attribute(string Name, int Size, int Offset, int Stride, int Index, VertexAttribPointerType Type, bool Normalised)
+        {
+            name = Name;
+            size = Size;
+            offset = Offset;
+            stride = Stride;
+            index = Index;
+            type = Type;
+            normalised = Normalised;
+        }
+
+        public void SetAttribute()
+        {
+            GL.VertexAttribPointer(index, size, type, normalised, stride, offset);
+        }
+        public void EnableAttribute()
+        {
+            GL.EnableVertexAttribArray(index);
+        }
+        public void DisableAttribute()
+        {
+            GL.DisableVertexAttribArray(index);
+        }
+    }
+
+}
