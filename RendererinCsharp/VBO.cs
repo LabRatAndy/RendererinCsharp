6f2ae51e039f7988b13704e95c0f34a76e965036
me <me@LAPTOP>
2018-11-17 16:47:41 +0000

    define the VBO class

---
+++ RendererinCsharp/VBO.cs
@@ -0,0 +1,42 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+
+namespace RendererinCsharp
+{
+    public class VertexBufferObject : IDisposable
+    {
+        private int handle;
+        private float[] vertexData = null;
+        private int dataSize;
+
+        VertexBufferObject(float[] vertexdata, int size)
+        {
+
+        }
+        public void Bind()
+        {
+
+        }
+        public void BufferData(BufferUsageHint drawtype)
+        {
+
+        }
+        public void Draw(int first, int count)
+        {
+
+        }
+        public void UnBind()
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+        ~VertexBufferObject()
+        {
+
+        }
+    }
+
+}
